ef2e33ce137bc9ed7a9cbd025abe0ab8a45e1ee8
Akshira02 <97776090+Akshira02@users.noreply.github.com>
2024-08-11 16:16:58 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,7 +1,6 @@
 // background.js
 chrome.tabs.onCreated.addListener(function(tab) {
     // Check if the new tab is the default new tab page
-    chrome.tabs.update(tab.id, { url: "https://erudiasearch.com" });
-    }
+    chrome.tabs.update(tab.id, { url: "https://eearch.com" });
 });
   
