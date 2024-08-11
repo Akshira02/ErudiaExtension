074637727c8878e0db5bc147df3d8b06e145514b
Akshira02 <97776090+Akshira02@users.noreply.github.com>
2024-08-10 23:01:09 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,8 @@
 // background.js
 chrome.tabs.onCreated.addListener(function(tab) {
-    chrome.tabs.update(tab.id, { url: "https://erudiasearch.com" });
-  });
+    // Check if the new tab is the default new tab page
+    if (tab.url === 'chrome://newtab/' || tab.url === '' || tab.url === undefined) {
+        chrome.tabs.update(tab.id, { url: "https://erudiasearch.com" });
+    }
+});
   
