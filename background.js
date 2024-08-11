fbf80f1c49351e6ce6b725129df9584ef3a80712
Akshira02 <97776090+Akshira02@users.noreply.github.com>
2024-08-11 17:19:06 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,15 @@
-// background.js
 chrome.tabs.onCreated.addListener(function(tab) {
-    // Check if the new tab is the default new tab page
-    chrome.tabs.update(tab.id, { url: "https://erudiasearch.com" });
+    // Check if the tab is the default new tab (empty, undefined, or chrome://newtab/)
+    if (tab.url === 'chrome://newtab/' || tab.url === '' || tab.url === undefined) {
+        chrome.tabs.update(tab.id, { url: "https://erudiasearch.com" });
+    }
 });
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    // If the tab is being updated and the URL is blank or undefined, redirect to ErudiaSearch.
+    if (changeInfo.status === 'loading' && (tab.url === '' || tab.url === undefined)) {
+        chrome.tabs.update(tabId, { url: "https://erudiasearch.com" });
+    }
+});
+
   
