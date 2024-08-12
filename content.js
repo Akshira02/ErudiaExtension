eb2c4a19c842c657cd73fa1c467cc66d4e7930b2
Akshira02 <97776090+Akshira02@users.noreply.github.com>
2024-08-11 21:07:43 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
 // Check if this is a new tab and redirect to ErudiaSearch
 if (window.location.href === 'chrome://newtab/') {
-    window.location.href = "https://erudiasearch.com";
+    window.location.replace("https://erudiasearch.com");
 }
