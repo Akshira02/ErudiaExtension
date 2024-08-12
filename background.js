8333b84a21f834ca6d12f21da418be6ab289ab5a
Akshira02 <97776090+Akshira02@users.noreply.github.com>
2024-08-11 21:01:10 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,17 +1,4 @@
 chrome.tabs.onCreated.addListener(function(tab) {
-    // Check if the tab URL is undefined or the default new tab page, and not already on ErudiaSearch
-    if ((tab.url === 'chrome://newtab/' || tab.url === '' || tab.url === undefined) &&
-        (!tab.pendingUrl || !tab.pendingUrl.startsWith("https://erudiasearch.com"))) {
-        chrome.tabs.update(tab.id, { url: "https://erudiasearch.com" });
-    }
-});
-
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    // Avoid redirection loop and only redirect if the tab is loading and not a Chrome internal page
-    if (changeInfo.status === 'loading' &&
-        (tab.url === 'chrome://newtab/' || tab.url === '' || tab.url === undefined) &&
-        (!tab.pendingUrl || !tab.pendingUrl.startsWith("https://erudiasearch.com"))) {
-        chrome.tabs.update(tabId, { url: "https://erudiasearch.com" });
-    }
+    // If a new tab is created, we don't need to do anything here since content script will handle the redirection.
 });
   
