21285e2abf68d86413305e48d50ebebeb61ce81a
Akshira02 <97776090+Akshira02@users.noreply.github.com>
2024-08-25 12:37:33 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,4 +1,5 @@
-// Check if this is a new tab and redirect to ErudiaSearch
+/* Check if this is a new tab and redirect to ErudiaSearch
 if (window.location.href === 'chrome://newtab/') {
     window.location.replace("https://erudiasearch.com");
 }
+*/
