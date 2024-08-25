5316dbfa3253a1df536a6f79f42482793bf2fcd8
Akshira02 <97776090+Akshira02@users.noreply.github.com>
2024-08-25 12:38:01 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,4 +1,20 @@
 chrome.tabs.onCreated.addListener(function(tab) {
-    // If a new tab is created, we don't need to do anything here since content script will handle the redirection.
+    console.log("Tab created with URL:", tab.url, "Pending URL:", tab.pendingUrl);
+    setTimeout(() => {
+        if (tab.url === 'chrome://newtab/' || (tab.pendingUrl === 'chrome://newtab/')) {
+            chrome.tabs.update(tab.id, { url: "https://erudiasearch.com" });
+        }
+    }, 100); // 100 milliseconds delay
 });
-  
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    console.log("Tab updated with URL:", tab.url);
+    if (changeInfo.status === 'complete') {
+        if (tab.url.startsWith('chrome://') || tab.url.startsWith('chrome-search://')) {
+            return; // Don't redirect Chrome internal pages
+        }
+        if (tab.url === 'chrome://newtab/' || (tab.pendingUrl && tab.pendingUrl === 'chrome://newtab/')) {
+            chrome.tabs.update(tabId, { url: "https://erudiasearch.com" });
+        }
+    }
+});
